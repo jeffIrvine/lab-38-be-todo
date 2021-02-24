bfc80c5bb8cc019d8362af68ebf0bdb3c0560050
jeffIrvine <jirvine1212@gmail.com>
2021-02-23 18:11:44 -0800

    test passing and insert working

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -5,6 +5,23 @@ const app = require('../lib/app');
 
 describe('lab-38-be-todo routes', () => {
   beforeEach(() => {
-    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'))
+    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
+  });
+
+  it('creates a todo with post', async() => {
+    const res = await request(app)
+      .post('/api/v1/todos')
+      .send({
+        title: 'Something Cheeky',
+        description: 'Something descriptive'
+      });
+
+    expect(res.body).toEqual({
+      id: '1',
+      title: 'Something Cheeky',
+      description: 'Something descriptive',
+      createdOn: expect.any(String) 
+    });
+
   });
 });

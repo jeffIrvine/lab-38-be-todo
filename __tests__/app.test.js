ea395b7ea6ab5581df20b303b747e068c1eda8bf
jeffIrvine <jirvine1212@gmail.com>
2021-02-25 15:31:59 -0800

    working get along with testing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const pool = require('../lib/utils/pool');
 const request = require('supertest');
 const app = require('../lib/app');
+const Todo = require('../lib/models/Todo');
 
 describe('lab-38-be-todo routes', () => {
   beforeEach(() => {
@@ -23,5 +24,46 @@ describe('lab-38-be-todo routes', () => {
       createdOn: expect.any(String) 
     });
 
+  });
+
+  it('gets all todos with GET', async() => {
+    await Promise.all([
+      {
+        title: 'test1',
+        description: 'test1'
+      },
+      {
+        title: 'test2',
+        description: 'test2'
+      },
+      {
+        title: 'test3',
+        description: 'test3'
+      }
+    ].map(todo => Todo.insert(todo)));
+
+    const res = await request(app)
+      .get('/api/v1/todos');
+
+    expect(res.body).toEqual([
+      {
+        id: expect.any(String),
+        title: 'test1',
+        description: 'test1',
+        createdOn: expect.any(String)
+      },
+      {
+        id: expect.any(String),
+        title: 'test2',
+        description: 'test2',
+        createdOn: expect.any(String)
+      },
+      {
+        id: expect.any(String),
+        title: 'test3',
+        description: 'test3',
+        createdOn: expect.any(String)
+      },
+    ]);
   });
 });
